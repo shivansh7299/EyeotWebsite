45693f4e3bdbea9a1f8626b4e4c1b61568dfc891
shivansh7299 <sharma.shivansh001@gmail.com>
2022-03-20 23:54:41 +0530

    mail and server

--- js/enquiry.js
+++ js/enquiry.js
@@ -4,9 +4,13 @@ let email = document.getElementById('emailID');
 let date = document.getElementById('date1');
 let time = document.getElementById('time1');
 
+
 booking.addEventListener('submit', (e)=>{
     e.preventDefault();
+    let select = document.getElementById('form-select').selectedIndex;
+    let optionSelect = document.getElementsByTagName('option');
     let appointment = {
+        option: optionSelect[select].value,
         name: uname.value,
         userEmail : email.value,
         date: date.value,
